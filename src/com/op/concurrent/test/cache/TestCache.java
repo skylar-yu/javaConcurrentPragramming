658510ac3e0ea7a11355ccb81084cd18b71e9817
yutianci <745369408@qq.com>
2017-10-11 20:11:39 +0800

    code commit

--- src/com/op/concurrent/test/cache/TestCache.java
+++ src/com/op/concurrent/test/cache/TestCache.java
@@ -4,15 +4,18 @@ package com.op.concurrent.test.cache;
  * Created by Administrator on 2017/9/22.
  */
 public class TestCache {
+    private static final Computable<Integer,String> computable = new ResolvePrime();
+    private static final Memoizer<Integer, String> memoizer = new Memoizer<>(computable);
     public static void main(String[] args) {
+//        Computable<Integer,String> computable = new ResolvePrime();
+//        Memoizer<Integer, String> memoizer = new Memoizer<>(computable);
 
-        Computable<Integer,String> computable = new ResolvePrime();
-        Memoizer<Integer, String> memoizer = new Memoizer<>(computable);
         String result = memoizer.compute(12);
         System.out.println("result is:"+result);
         String result2 = memoizer.compute(13);
         System.out.println("result2 is:"+result2);
         String result3 = memoizer.compute(12);
         System.out.println("result3 is:"+result3);
+        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
