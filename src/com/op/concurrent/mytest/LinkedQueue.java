b4399f36f30d9d7fa6000e6790c9a8fae0e612d9
skylar-yu <745369408@qq.com>
2017-11-01 18:26:07 +0800

    Non-blocking algorithms

--- src/com/op/concurrent/mytest/LinkedQueue.java
+++ src/com/op/concurrent/mytest/LinkedQueue.java
@@ -25,18 +25,29 @@ public class LinkedQueue<E> {
     public boolean put(E item) {
         Node<E> newNode = new Node<E>(item, null);
         while (true) {
-            Node<E> curTail = tail.get();
-            Node<E> residue = curTail.next.get();
-            if (curTail == tail.get()) {
-                if (residue == null) { /* A */
-                    if (curTail.next.compareAndSet(null, newNode)) {/* C */
-                        tail.compareAndSet(curTail, newNode) /* D */;
+            Node<E> currentNode = tail.get();
+            Node<E> nextPoint = currentNode.next.get();
+            //此处判断是为了，两个线程同时进入该方法，第一个线程成功执行下面的代码；第二个线程才开始执行
+            //不判断，if(nextPoint==null)成立，但是if(currentNode.next.compareAndSet(null, newNode))不成立
+            //所以此处判断只是为了节省性能
+            //所以此处判断是为了保证当前线程获取到的链表的静止状态是唯一的，并不是当前其他线程执行成功执行同样的方法后的静止状态
+            if (currentNode == tail.get()) {
+                //判断是否为中间状态
+                if (nextPoint == null) { /* A */
+                    //不为中间状态
+                    if (currentNode.next.compareAndSet(null, newNode)) {/* C */
+                        tail.compareAndSet(currentNode, newNode) /* D */;
                         return true;
                     }
                 } else {
-                    tail.compareAndSet(curTail, residue) /* B */;
+                    //为中间状态
+                    tail.compareAndSet(currentNode, nextPoint) /* B */;
                 }
             }
         }
     }
+    /**
+     *  插入节点到尾部 包含两个CAS操作： 1.将新节点连接到尾部  2.tail指向新的节点
+     *  ThreadA想要插入节点到尾部；
+     */
 }
