ed2ff5bdc00204eb6bb0ef16d16a30c165a3eebc
skylar-yu <745369408@qq.com>
2017-11-01 18:40:15 +0800

    Non-blocking algorithms

--- src/com/op/concurrent/mytest/LinkedQueue.java
+++ src/com/op/concurrent/mytest/LinkedQueue.java
@@ -27,7 +27,7 @@ public class LinkedQueue<E> {
         while (true) {
             Node<E> currentNode = tail.get();
             Node<E> nextPoint = currentNode.next.get();
-            //此处判断是为了，两个线程同时进入该方法，第一个线程成功执行下面的代码；第二个线程才开始执行
+            //此处判断是为了，两个线程同时进入该方法，第一个线程成功执行上面的代码和下面的代码；第二个线程已经执行的上面的代码，才开始执行以下代码
             //不判断，if(nextPoint==null)成立，但是if(currentNode.next.compareAndSet(null, newNode))不成立
             //所以此处判断只是为了节省性能
             //所以此处判断是为了保证当前线程获取到的链表的静止状态是唯一的，并不是当前其他线程执行成功执行同样的方法后的静止状态
